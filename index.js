37e0457143a940bf89274f825cd56980aebd28fc
Bret Petersen <bretpetersen82@gmail.com>
2021-10-11 22:29:16 -0500

    General questions for each section are added

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// PSUEDO CODING // Generate README.md with
+// Title of my project and sections entitled Description, Table of Contents,
+// Installation, Usage, License, Contributing, Tests, and Questions
+
 // TODO: Include packages needed for this application
 
 const inquirer = require("inquirer");
@@ -5,20 +9,112 @@ const fs = require("fs");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [
+let questions = [
   {
     type: "input",
-    message: "...",
-    name: "...",
+    message: "Title - What is the title of your project?\n",
+    name: "projTitle",
+  },
+  {
+    type: "input",
+    message:
+      "Desription - Describe your project. (answer as many of these questions as possible)\nWhat was your motivation?\nWhy was it built?\nWhat problem does it solve?\nWhat did you learn?\n",
+    name: "projDescription",
+  },
+  {
+    type: "input",
+    message:
+      "Installation - What are the steps required to install your project?\nProvide a step-by-step description of how to get the development environment running.\n",
+    name: "projInstallation",
+  },
+  {
+    type: "input",
+    message: "Usage - Provide instructions and examples for use.\n",
+    name: "projUsage",
+  },
+  {
+    type: "list",
+    message:
+      "License - If you would like to include a license, please choose one.\n",
+    name: "projLicense",
+    choices: [
+      "Apache License 2.0",
+      "MIT License",
+      "Creative Commons Zero v1.0 Universal",
+      "Mozilla Public License 2.0",
+    ],
+  },
+  {
+    type: "input",
+    message:
+      "Contribute - If you are open to contributing on this project, please provide instructions and/or rules to contribute.\n",
+    name: "projContribute",
+  },
+  {
+    type: "input",
+    message:
+      "Tests - If you have any tests you would like to describe to users, explan them here.\n",
+    name: "projTests",
+  },
+  {
+    type: "input",
+    message:
+      "Questions - Are there any questions remaining about this project? Include them here.\n",
+    name: "projQuestions",
   },
 ];
+// inquirer
+//   .prompt([
+//     {
+//       type: "input",
+//       message: "What is the title of your project?",
+//       name: "projTitle",
+//     },
+//     {
+//       type: "input",
+//       message:
+//         "Please describe your project. What was your motivation? Why was it built? What problem does it solve? What did you learn?",
+//       name: "projDescription",
+//     },
+//     {
+//       type: "password",
+//       message: "Re-enter password to confirm:",
+//       name: "confirm",
+//     },
+//   ])
+//   .then((response) =>
+//     response.confirm === response.password
+//       ? console.log("Success!")
+//       : console.log("You forgot your password already?!")
+//   );
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {
-  inquirer.prompt(questions).then(responses);
+  // inquirer.prompt(questions).then((answers) => {
+  //   console.log(`Here are your answers:\n
+  //   Title: ${projTitle}\n
+  //   Description: ${projDescription}\n
+  //   Installation: ${projInstallation}\n
+  //   Usage: ${projUsage}\n
+  //   License: ${projLicense}\n
+  //   Contribute: ${projContribute}\n
+  //   Tests: ${projTests}\n
+  //   Questions: ${projQuestions}\n`);
+  // });
+  inquirer.prompt(questions).then((answers) => {
+    console.log(`Here are your answers:\n
+    Title: ${answers.projTitle}\n
+    Description: ${answers.projDescription}\n
+    Installation: ${answers.projInstallation}\n
+    Usage: ${answers.projUsage}\n
+    License: ${answers.projLicense}\n
+    Contribute: ${answers.projContribute}\n
+    Tests: ${answers.projTests}\n
+    Questions: ${answers.projQuestions}\n`);
+  });
 }
 
 // Function call to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,8 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  return `string of markdown of chosen badge`;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -11,6 +13,7 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
+// data is the response object
 function generateMarkdown(data) {
   return `# ${data.title}
 
